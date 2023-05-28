a20654826bb061e080ed16c99e6cfd9eca66c6a8
Ramill <ramill_ibragimov@icloud.com>
2023-05-28 19:22:45 +0300

    Initial Commit

---
+++ FavoritePlaces/FavoritePlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritePlacesApp.swift
+//  FavoritePlaces
+//
+//  Created by Ramill Ibragimov on 28.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoritePlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
