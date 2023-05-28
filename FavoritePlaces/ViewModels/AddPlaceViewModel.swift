afe9f34de82151b340102034c7124b4bab27c484
Ramill <ramill_ibragimov@icloud.com>
2023-05-28 20:16:54 +0300

    AddPlaceViewModel

---
+++ FavoritePlaces/ViewModels/AddPlaceViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  AddPlaceViewModel.swift
+//  FavoritePlaces
+//
+//  Created by Ramill Ibragimov on 28.05.2023.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+class AddPlaceViewModel: ObservableObject {
+    @Published var name: String = ""
+    @Published var city: String = ""
+    @Published var country: String = ""
+    @Published var notes: String = ""
+    
+    @Published var showProgress: Bool = false
+    
+    @Published var image: Image?
+    @Published var imageData: Data?
+    
+    func savePlace() {
+        
+    }
+    
+    func getImageFor(placeName: String) async {
+        let encodedName = placeName.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!
+        let url = URL(string: "https://source.unsplash.com/1080x1350/?\(encodedName)")!
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            await MainActor.run(body: {
+                imageData = data
+                image = Image(uiImage: UIImage(data: data)!)
+            })
+        } catch let err {
+            print(err.localizedDescription)
+        }
+    }
+}
