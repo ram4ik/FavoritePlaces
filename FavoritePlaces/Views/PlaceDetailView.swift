9e27429b9c9783434abfb2bd49dd598620de7e04
Ramill <ramill_ibragimov@icloud.com>
2023-05-29 08:40:29 +0300

    placeDetailsView

--- FavoritePlaces/Views/PlaceDetailView.swift
+++ FavoritePlaces/Views/PlaceDetailView.swift
@@ -23,7 +23,7 @@ struct PlaceDetailView: View {
         ZStack(alignment: .top) {
             ZStack(alignment: .bottom) {
                 backgroundImageView()
-                // placeDetailsView()
+                placeDetailsView()
             }
             .frame(maxHeight: .infinity)
             .edgesIgnoringSafeArea(.all)
@@ -65,6 +65,48 @@ struct PlaceDetailView: View {
             
             Spacer()
         }
+    }
+    
+    fileprivate func placeDetailsView() -> some View {
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .leading, spacing: 20) {
+                HStack {
+                    Text(place.name)
+                        .font(.title)
+                    Spacer()
+                }
+                
+                HStack {
+                    Text(place.city)
+                    Spacer()
+                    Text(place.country)
+                }
+                .foregroundStyle(.secondary)
+                
+                Text(place.notes)
+                
+                Spacer()
+                
+                Map(coordinateRegion: $locationManager.region, interactionModes: .zoom)
+                    .frame(height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Divider()
+                
+                Button(role: .destructive) {
+                    CoreDataManager.shared.delete(id: place.id)
+                    dismiss()
+                } label: {
+                    Text("Delete")
+                }
+                .frame(maxWidth: .infinity)
+                .buttonStyle(.borderedProminent)
+            }.padding(10)
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .frame(height: 400)
+        .background(.thinMaterial, in: RoundedRectangle(cornerRadius: 20))
     }
 }
 
